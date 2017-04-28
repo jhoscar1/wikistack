e8db2c18d3e14af08ccf941d066b99ac814292bc
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-04-28 17:23:55 -0400

    adding validators to db fields'

--- models/index.js
+++ models/index.js
@@ -5,21 +5,27 @@ var db = new Sequelize('postgres://localhost:5432/wikistack', {
 
 const User = db.define('user', {
    name: {
-       type: Sequelize.STRING
+       type: Sequelize.STRING,
+       allowNull: false
    },
    email: {
-       type: Sequelize.STRING
+       type: Sequelize.STRING,
+       allowNull: false,
+       isEmail: true
    }});
 
 const Page = db.define('page', {
    title: {
-       type: Sequelize.STRING
+       type: Sequelize.STRING,
+       allowNull: false
    },
    urlTitle: {
-       type: Sequelize.STRING
+       type: Sequelize.STRING,
+       allowNull: false
    },
    content: {
        type: Sequelize.TEXT
+       allowNull: false
    },
    status: {
        type: Sequelize.ENUM('open', 'closed')
