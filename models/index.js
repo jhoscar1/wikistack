e4bb8fdccc3c36780fb56c92f335c20ce7073fd7
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-04-28 17:10:02 -0400

    setting up models

---
+++ models/index.js
@@ -0,0 +1,25 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const User = db.define('user', {
+   name: {
+       type: Sequelize.STRING
+   },
+   email: {
+       type: Sequelize.STRING
+   }});
+
+const Page = db.define('page', {
+   title: {
+       type: Sequelize.STRING
+   },
+   urlTitle: {
+       type: Sequelize.STRING
+   },
+   content: {
+       type: Sequelize.TEXT
+   },
+   status: {
+       type: Sequelize.ENUM('open', 'closed')
+   }
+});
